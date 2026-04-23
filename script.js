0f900e604b37854475541dafe5fefa9a756b4c67
Milan-Vialva <milan.vialva06@gmail.com>
2026-04-23 13:14:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("mousemove", function(e) {
+    const trail = document.createElement("div");
+    trail.className = "trail";
+
+    trail.style.left = e.pageX + "px";
+    trail.style.top = e.pageY + "px";
+
+    document.body.appendChild(trail);
+
+    // Remove after animation ends
+    setTimeout(() => {
+        trail.remove();
+    }, 500);
+});
