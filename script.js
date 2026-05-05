0c728d91949a854dca09e81bd4d05e28a420e3f4
Milan-Vialva <milan.vialva06@gmail.com>
2026-05-04 23:24:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const maxTrail = 25;        // limit number of dots
-const trails = [];
-let lastSpawn = 0;
-const spawnDelay = 20;      // ms between dots
-
-document.addEventListener("mousemove", (e) => {
-    const now = Date.now();
-
-    // throttle how often dots spawn
-    if (now - lastSpawn < spawnDelay) return;
-    lastSpawn = now;
-
-    const trail = document.createElement("div");
-    trail.className = "trail";
-
-    trail.style.left = e.pageX + "px";
-    trail.style.top = e.pageY + "px";
-
-    document.body.appendChild(trail);
-    trails.push(trail);
-
-    // remove oldest if too many
-    if (trails.length > maxTrail) {
-        const old = trails.shift();
-        old.remove();
-    }
-
     // auto remove after animation
     setTimeout(() => {
         trail.remove();
